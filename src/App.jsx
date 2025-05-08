e6ab693d3d5b8c96d4da96af3936868748e3cf2d
Harshita Nahata <khushinahata04@gmail.com>
2025-05-08 13:13:55 +0530

    stats component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './components/about/About';
 import Features from './components/features/Features';
 import Menu from './components/menu/Menu';
 import Choose from './components/choose/Choose';
+import Stats from './components/stats/Stats';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Features />
       <Menu />
       <Choose />
+      <Stats />
     </>
   );
 }
