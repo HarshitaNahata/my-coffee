da7bb8dbb040499f447976e31141df4e9ae70088
Harshita Nahata <khushinahata04@gmail.com>
2025-07-11 00:49:54 +0530

    made it responsive for small screens too

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/logo.png';
 import { links } from '../../Data';
 import { FaStream } from "react-icons/fa";
 import './header.css';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleToggle = () => setMenuOpen(!menuOpen);
+
+    // Optional: Close menu when a link is clicked (for better UX)
+    const handleLinkClick = () => setMenuOpen(false);
+
     return (
         <header className='header'>
             <nav className='nav container'>
                 <a href='/' className='nav__logo'>
                     <img src={logo} alt='' className='nav__logo-img' />
                 </a>
-                <div className="nav__menu">
+                <div className={`nav__menu${menuOpen ? ' active' : ''}`}>
                     <ul className="nav__list">
-                        {links.map(({ name, path }, index) => {
-                            return (
-                                <li className="nav__item" key={index}>
-                                    <a href={path} className="nav__link">{name}</a>
-                                </li>
-                            )
-                        })}
+                        {links.map(({ name, path }, index) => (
+                            <li className="nav__item" key={index}>
+                                <a href={path} className="nav__link" onClick={handleLinkClick}>
+                                    {name}
+                                </a>
+                            </li>
+                        ))}
                     </ul>
                 </div>
-
-                <div className="nav__toggle">
+                <div className="nav__toggle" onClick={handleToggle}>
                     <FaStream />
                 </div>
             </nav>
